0a633467dc82fb0d8b9cb7161c9f9b5e58d6dcb9
NiloLP <112017951+NiloLP@users.noreply.github.com>
2023-07-24 14:52:37 +0000

    esto va pa render

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,48 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/")
 public class MainController {
-    
+    @Autowired
+    private CursoRepository repository;
+
+    @GetMapping(path = "/")
+    public @ResponseBody String home(){return "Luffy - Monkey D Luffy";}
+
+    @GetMapping(path = "/codigo")
+    public @ResponseBody String codigo(){return "Luffy";}
+
+    @GetMapping(path = "/nombre-completo")
+    public @ResponseBody String nombre(){return "Monkey D Luffy";}
+
+    @PostMapping(path = "/api/curso/nuevo")
+    public @ResponseBody String nuevo(@RequestParam String nombre, @RequestParam Integer creditos) {
+        Curso n = new Curso();
+        n.setNombre(nombre);
+        n.setCreditos(creditos);
+        repository.save(n);
+        return "Curso Guardado";
+    }
+
+    @DeleteMapping(path = "/api/curso/eliminar")
+    public @ResponseBody String delUser(@RequestParam Integer id){
+        Curso n = new Curso();
+        n.setId(id);
+        repository.delete(n);
+        return "Curso eliminado";
+    }
+
+    @GetMapping(path = "/api/curso/listar")
+    public @ResponseBody Iterable<Curso> listar(){
+        return repository.findAll();
+    }
 }
